8740efb16e3be91a5447ae7b96fffc7bed91bffb
Wowsaruss <russell.hayes.dev@gmail.com>
2025-05-07 08:58:13 -0700

    add user encryption and login

--- server/database/user-queries.js
+++ server/database/user-queries.js
@@ -1,8 +1,15 @@
 const knex = require("./connection.js");
 
-async function create(first_name, last_name, email, password) {
-    const results = await knex('users').insert({ first_name, last_name, email, password }).returning('*');
-    return results[0];
+async function get(email) {
+  const results = await knex("users").where("email", email);
+  return results[0];
 }
 
-module.exports = { create }
+async function create(first_name, last_name, email, password) {
+  const results = await knex("users")
+    .insert({ first_name, last_name, email, password })
+    .returning("*");
+  return results[0];
+}
+
+module.exports = { get, create };
--- server/server.js
+++ server/server.js
@@ -1,20 +1,27 @@
-const app = require('./server-config.js');
-const routes = require('./server-routes.js');
+require("dotenv").config();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const app = require("./server-config.js");
+const routes = require("./server-routes.js");
+const users = require("./database/user-queries.js");
 
 const port = process.env.PORT || 5000;
 
-app.post('/users', routes.postUser);
+app.post("/login", routes.loginUser);
 
-app.get('/', routes.getAllTodos);
-app.get('/:id', routes.getTodo);
+app.post("/users", routes.postUser);
+app.get("/users/:email", routes.postUser);
 
-app.post('/', routes.postTodo);
-app.patch('/:id', routes.patchTodo);
+app.get("/", routes.getAllTodos);
+app.get("/:id", routes.getTodo);
 
-app.delete('/', routes.deleteAllTodos);
-app.delete('/:id', routes.deleteTodo);
+app.post("/:user_id", routes.postTodo);
+app.patch("/:id", routes.patchTodo);
 
-if (process.env.NODE_ENV !== 'test') {
+app.delete("/", routes.deleteAllTodos);
+app.delete("/:id", routes.deleteTodo);
+
+if (process.env.NODE_ENV !== "test") {
   app.listen(port, () => console.log(`Listening on port ${port}`));
 }
 
